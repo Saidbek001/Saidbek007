ca9f284c3ea1b329b1f73393ef14aebc587a3c86
Ulugbek Gayibov <Saidbek007.com>
2025-10-02 19:40:54 +0500

    print

--- Lesson.py
+++ Lesson.py
@@ -1,7 +1,12 @@
-jfjejfhrjr:
-print('hello')
-ghnbhj
-hjbhj
-gvujijuhugu
-hbbbuhihu
-nbybubub
+login = ($@!|}007)
+password = (5891)
+
+if (login) == ($@!|}007):
+    print("True Login")
+else:
+    print("False login")
+
+if (password) == (5891):
+    print("True password")
+else:
+    print("False password")
